41fb9a3fb76390f5a6247f44952ca3ec650717da
Aitzol Naberan Burgaña <anaberan@gmail.com>
2022-12-09 17:44:20 +0100

    Froga repoa

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const fetch = (...args) =>
+	import('node-fetch').then(({default: fetch}) => fetch(...args));
+const app = express();
+
+app.set('view engine', 'pug')
+const RADIO_DATA_URL = "https://raw.githubusercontent.com/erral/eitbapi/master/radio-cache.json"
+const TV_DATA_URL = ""
+const RADIO_DATA = fetch(RADIO_DATA_URL, {method: 'GET'})
+.then(res => res.json())
+
+app.get('/', function (req, res) {
+    res.render('templates/index')
+  });
+app.get('/playlist', (req, res) => {
+    RADIO_DATA
+} )
+app.get('/program-type-list', (req,res) => {
+
+    res.json({
+        "@context": "http://www.w3.org/ns/hydra/context.jsonld",
+        "@id": '/program-type-list',
+        "@type": "TypeList",
+        "parent": '/',
+        "member": [],
+    })
+}
+
+)
+
+app.listen(3000, () => {
+ console.log("El servidor está inicializado en el puerto 3000");
+});
