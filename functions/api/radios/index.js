ac68f5b97d0938fb7461d180ebb9a3ba8c0b1295
Aitzol Naberan Burgaña <anaberan@gmail.com>
2022-12-10 09:52:01 +0100

    Irrati zerrenda

---
+++ functions/api/radios/index.js
@@ -0,0 +1,35 @@
+export async function onRequestGet(context) {
+    const {
+      request, // same as existing Worker API
+      env, // same as existing Worker API
+      params, // if filename includes [id] or [[path]]
+      waitUntil, // same as ctx.waitUntil in existing Worker API
+      next, // used for middleware or to fetch assets
+      data, // arbitrary space for passing data between middlewares
+    } = context;
+        const RADIOS = [
+            {id: 'euskadi_irratia', title: 'Euskadi irratia'},
+            {id: 'radio_euskadi', title: 'Radio euskadi'},
+            {id: 'radio_vitoria', title: 'Radio Vitoria'},
+            {id: 'gaztea', title: 'Gaztea'}
+        ].map((radio) => {
+            return {
+            '@id': '/radios/' + radio[id],
+            '@type': 'Radio Station Program list',
+            'parent': '/radios',
+            'title': radio[title]
+            }
+        })
+        const result = {
+            '@context': "http://www.w3.org/ns/hydra/context.jsonld",
+            '@id': "/radio",
+            '@type': "RadioStationList",
+            parent: "/",
+            member: RADIOS
+        }
+        return new Response(JSON.stringify(result)  , {
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+          },
+        });
+      }
