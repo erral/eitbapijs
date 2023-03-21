555e08199c65fb4e7cd9fd6e2a3ea32964a07d0a
Urko Tamayo <utamayo@uni.eus>
2023-03-21 11:14:22 +0100

    Cors added to constants.js and index.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const HEADERS = {
-  headers: { 'content-type': 'application/json;charset=UTF-8' },
+  headers: { 'content-type': 'application/json;charset=UTF-8', 'Access-Control-Allow-Origin': '*' },
 };
 export const RADIO = 'radios';
 
